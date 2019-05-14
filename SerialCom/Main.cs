0882252b75698446062efda42c9141173cfb9d91
xdawax <david.carlsson.6652@student.uu.se>
2019-05-14 08:51:28 +0200

    Change in db from int to tinyint

--- SerialCom/Main.cs
+++ SerialCom/Main.cs
@@ -66,12 +66,12 @@ namespace SerialCom
 
         private void transmitACK(Packet packet)
         {
+            if (!mySerialPort.IsOpen) return;
             packet.ToACK();
             byte[] buf = packet.PacketAsBuf();
             Int32 size = buf.Length;
 
             mySerialPort.Write(buf, 0 ,size);
-
         }
 
         public void displayPacketContent(Packet packet)
