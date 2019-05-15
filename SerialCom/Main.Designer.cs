46b301356a3734fed0a5cd9b87a6d88e0e886d4d
xdawax <david.carlsson.6652@student.uu.se>
2019-05-15 16:13:10 +0200

    Remove redundancy

--- SerialCom/Main.Designer.cs
+++ SerialCom/Main.Designer.cs
@@ -28,32 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonSend = new System.Windows.Forms.Button();
             this.buttonClear = new System.Windows.Forms.Button();
-            this.richTextBoxTX = new System.Windows.Forms.RichTextBox();
             this.richTextBoxRX = new System.Windows.Forms.RichTextBox();
-            this.checkBoxIM = new System.Windows.Forms.CheckBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listBoxComPorts = new System.Windows.Forms.ListBox();
-            this.buttonSelectCom1 = new System.Windows.Forms.Button();
             this.buttonDisplaySensors = new System.Windows.Forms.Button();
-            this.buttonDebug = new System.Windows.Forms.Button();
-            this.buttonSelectCom2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // buttonSend
-            // 
-            this.buttonSend.Location = new System.Drawing.Point(617, 12);
-            this.buttonSend.Name = "buttonSend";
-            this.buttonSend.Size = new System.Drawing.Size(75, 23);
-            this.buttonSend.TabIndex = 0;
-            this.buttonSend.Text = "Send";
-            this.buttonSend.UseVisualStyleBackColor = true;
-            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
             // 
             // buttonClear
             // 
-            this.buttonClear.Location = new System.Drawing.Point(295, 414);
+            this.buttonClear.Location = new System.Drawing.Point(292, 383);
             this.buttonClear.Name = "buttonClear";
             this.buttonClear.Size = new System.Drawing.Size(75, 23);
             this.buttonClear.TabIndex = 1;
@@ -61,129 +43,44 @@
             this.buttonClear.UseVisualStyleBackColor = true;
             this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
-            // richTextBoxTX
-            // 
-            this.richTextBoxTX.Location = new System.Drawing.Point(54, 12);
-            this.richTextBoxTX.Name = "richTextBoxTX";
-            this.richTextBoxTX.Size = new System.Drawing.Size(557, 40);
-            this.richTextBoxTX.TabIndex = 2;
-            this.richTextBoxTX.Text = "";
-            this.richTextBoxTX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBoxTX_KeyPress);
-            // 
             // richTextBoxRX
             // 
-            this.richTextBoxRX.Location = new System.Drawing.Point(54, 58);
+            this.richTextBoxRX.Location = new System.Drawing.Point(54, 27);
             this.richTextBoxRX.Name = "richTextBoxRX";
             this.richTextBoxRX.Size = new System.Drawing.Size(557, 350);
             this.richTextBoxRX.TabIndex = 3;
             this.richTextBoxRX.Text = "";
             // 
-            // checkBoxIM
-            // 
-            this.checkBoxIM.AutoSize = true;
-            this.checkBoxIM.Location = new System.Drawing.Point(617, 35);
-            this.checkBoxIM.Name = "checkBoxIM";
-            this.checkBoxIM.Size = new System.Drawing.Size(38, 17);
-            this.checkBoxIM.TabIndex = 4;
-            this.checkBoxIM.Text = "IM";
-            this.checkBoxIM.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(614, 266);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(28, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Com";
-            // 
-            // listBoxComPorts
-            // 
-            this.listBoxComPorts.FormattingEnabled = true;
-            this.listBoxComPorts.Location = new System.Drawing.Point(617, 282);
-            this.listBoxComPorts.Name = "listBoxComPorts";
-            this.listBoxComPorts.Size = new System.Drawing.Size(75, 95);
-            this.listBoxComPorts.TabIndex = 6;
-            this.listBoxComPorts.SelectedIndexChanged += new System.EventHandler(this.listBoxComPorts_SelectedIndexChanged);
-            // 
-            // buttonSelectCom1
-            // 
-            this.buttonSelectCom1.Location = new System.Drawing.Point(698, 282);
-            this.buttonSelectCom1.Name = "buttonSelectCom1";
-            this.buttonSelectCom1.Size = new System.Drawing.Size(75, 23);
-            this.buttonSelectCom1.TabIndex = 7;
-            this.buttonSelectCom1.Text = "Port 1";
-            this.buttonSelectCom1.UseVisualStyleBackColor = true;
-            this.buttonSelectCom1.Click += new System.EventHandler(this.buttonSelectCom_Click);
-            // 
             // buttonDisplaySensors
             // 
-            this.buttonDisplaySensors.Location = new System.Drawing.Point(617, 101);
+            this.buttonDisplaySensors.Location = new System.Drawing.Point(617, 337);
             this.buttonDisplaySensors.Name = "buttonDisplaySensors";
-            this.buttonDisplaySensors.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisplaySensors.Size = new System.Drawing.Size(75, 40);
             this.buttonDisplaySensors.TabIndex = 8;
             this.buttonDisplaySensors.Text = "Sensor Data";
             this.buttonDisplaySensors.UseVisualStyleBackColor = true;
             this.buttonDisplaySensors.Click += new System.EventHandler(this.buttonDisplaySensors_Click);
-            // 
-            // buttonDebug
-            // 
-            this.buttonDebug.Location = new System.Drawing.Point(617, 167);
-            this.buttonDebug.Name = "buttonDebug";
-            this.buttonDebug.Size = new System.Drawing.Size(75, 40);
-            this.buttonDebug.TabIndex = 9;
-            this.buttonDebug.Text = "Send Debug Packet";
-            this.buttonDebug.UseVisualStyleBackColor = true;
-            this.buttonDebug.Click += new System.EventHandler(this.buttonDebug_Click);
-            // 
-            // buttonSelectCom2
-            // 
-            this.buttonSelectCom2.Location = new System.Drawing.Point(698, 311);
-            this.buttonSelectCom2.Name = "buttonSelectCom2";
-            this.buttonSelectCom2.Size = new System.Drawing.Size(75, 23);
-            this.buttonSelectCom2.TabIndex = 10;
-            this.buttonSelectCom2.Text = "Port 2";
-            this.buttonSelectCom2.UseVisualStyleBackColor = true;
-            this.buttonSelectCom2.Click += new System.EventHandler(this.ButtonSelectCom2_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.buttonSelectCom2);
-            this.Controls.Add(this.buttonDebug);
             this.Controls.Add(this.buttonDisplaySensors);
-            this.Controls.Add(this.buttonSelectCom1);
-            this.Controls.Add(this.listBoxComPorts);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.checkBoxIM);
             this.Controls.Add(this.richTextBoxRX);
-            this.Controls.Add(this.richTextBoxTX);
             this.Controls.Add(this.buttonClear);
-            this.Controls.Add(this.buttonSend);
             this.Name = "Main";
             this.Text = "Main";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button buttonSend;
         private System.Windows.Forms.Button buttonClear;
-        private System.Windows.Forms.RichTextBox richTextBoxTX;
         private System.Windows.Forms.RichTextBox richTextBoxRX;
-        private System.Windows.Forms.CheckBox checkBoxIM;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listBoxComPorts;
-        private System.Windows.Forms.Button buttonSelectCom1;
         private System.Windows.Forms.Button buttonDisplaySensors;
-        private System.Windows.Forms.Button buttonDebug;
-        private System.Windows.Forms.Button buttonSelectCom2;
     }
 }
 
--- SerialCom/Main.cs
+++ SerialCom/Main.cs
@@ -11,8 +11,6 @@ namespace SerialCom
         private const int baudRate = 38400;
         private const Parity parity = Parity.None;
         private const StopBits stopBits = StopBits.One;
-
-        private string comPort = "UNDEFINED";
         private List<SerialCom> connections = new List<SerialCom>();
 
         public Main()
@@ -21,9 +19,6 @@ namespace SerialCom
             InitConnections();
             OpenConnections();
             displayConnections();
-
-            buttonSend.Enabled = false;
-            checkBoxIM.Enabled = false;
         }
 
         private void InitConnections()
@@ -68,79 +63,6 @@ namespace SerialCom
             }
         }
 
-        private void populateComPorts()
-        {
-            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
-
-            // Display each port name to the console.
-            foreach (string port in ports)
-            {
-                listBoxComPorts.Items.Add(port);
-            }
-        }
-
-        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
-        {/*
-            if (!serialPort1.IsOpen) return;
-            byte received_byte = 0;
-            byte index = 0;
-            byte[] rxBuf = new byte[255];
-
-            while (received_byte != Packet.END_COM)
-            {
-                received_byte = (byte)serialPort1.ReadByte();
-                rxBuf[index] = received_byte;
-                index++;
-            }
-
-            handleData(rxBuf);
-            
-            this.Invoke(new EventHandler(displayText));*/
-        }
-
-        private void handleData(byte[] buf)
-        {/*
-            Packet packet = new Packet();
-
-            packet.Address = buf[Packet.BUF_ADDRESS];
-            packet.Type = (Packet.Sensor_t)buf[Packet.BUF_TYPE];
-            packet.Data = BitConverter.ToUInt32(buf, Packet.BUF_DATA0);
-            packet.Sequence = buf[Packet.BUF_SEQUENCE];
-            packet.CheckSum = buf[Packet.BUF_CHECKSUM];
-            byte debug_sum = packet.checkSum();
-
-            // if the checksum is correct store the packet in the db and send ack
-            if (packet.checkSum() == packet.CheckSum)
-            {
-                SensorDataAccess.SaveData(packet);
-                transmitACK(packet);
-            } else
-            {
-                transmitNACK(packet);
-            }*/
-        }
-
-        private void transmitNACK(Packet packet)
-        {/*
-            if (!serialPort1.IsOpen) return;
-            packet.ToNACK();
-
-            byte[] buf = packet.PacketAsBuf();
-            Int32 size = buf.Length;
-
-            serialPort1.Write(buf, 0, size);*/
-        }
-
-        private void transmitACK(Packet packet)
-        {/*
-            if (!serialPort1.IsOpen) return;
-            packet.ToACK();
-            byte[] buf = packet.PacketAsBuf();
-            Int32 size = buf.Length;
-
-            serialPort1.Write(buf, 0 ,size);*/
-        }
-
         public void displayPacketContent(Packet packet)
         {
             richTextBoxRX.AppendText(packet.Contents);
@@ -175,65 +97,22 @@ namespace SerialCom
 
         }
 
-        private void buttonSend_Click(object sender, EventArgs e)
-        {
-           // serialPort1.Write(richTextBoxTX.Text);
-        }
-
         private void buttonClear_Click(object sender, EventArgs e)
         {
-            richTextBoxTX.Clear();
             richTextBoxRX.Clear();
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
-            //serialPort1.Close();
+            foreach (SerialCom serialCom in connections)
+            {
+                if (serialCom.isOpen())
+                {
+                    serialCom.closePort();
+                }
+            }
         }
         
-        private void richTextBoxTX_KeyPress(object sender, KeyPressEventArgs e)
-        {/*
-            if (serialPort1.IsOpen && checkBoxIM.Checked)
-            {
-                char[] ch = new char[1];
-                ch[0] = e.KeyChar;
-                serialPort1.Write(ch, 0, 1);
-            }*/
-        }
-
-        private void listBoxComPorts_SelectedIndexChanged(object sender, EventArgs e)
-        { /*
-            if (listBoxComPorts.SelectedItem != null)
-            {
-                comPort = listBoxComPorts.SelectedItem.ToString();
-            }*/
-        }
-
-        private void buttonSelectCom_Click(object sender, EventArgs e)
-        { /*
-            if (serialPort1.PortName == comPort) {
-                return;
-            }
-
-            if (comPort == "UNDEFINED")
-            {
-                MessageBox.Show("No com port selected!");
-            }
-            else
-            {
-                if (serialPort1.IsOpen)
-                {
-                    serialPort1.Close();
-                }
-                
-                serialPort1.PortName = comPort;
-                serialPort1.Open();
-                serialPort1.DiscardInBuffer();
-                serialPort1.DiscardOutBuffer();
-                buttonSend.Enabled = true;
-                checkBoxIM.Enabled = true;
-            }*/
-        }
 
         private void buttonDisplaySensors_Click(object sender, EventArgs e)
         {
@@ -241,22 +120,7 @@ namespace SerialCom
             SensorData sensData = new SensorData(this);
             sensData.Show();
         }
-        /*
-        public void closeSerialPort()
-        {
-            serialPort1.Close();
-        }*/
 
-        private void buttonDebug_Click(object sender, EventArgs e)
-        {
-            Packet packet = new Packet();
-            packet.Address = 9;
-            packet.Type = Packet.Sensor_t.REED;
-            packet.Sequence = 123;
-            packet.ToACK();
-
-            transmitACK(packet);
-        }
 
         private void ButtonSelectCom2_Click(object sender, EventArgs e)
         {
